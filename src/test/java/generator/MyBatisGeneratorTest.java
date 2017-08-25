72f7386a692222e932db4a62625543dd3e77f36b
yepeng <yepeng@xsili.com>
2017-08-25 18:03:23 +0800

    补充提交(添加3个代码生成插件), 添加生成本地代码的测试类

---
+++ src/test/java/generator/MyBatisGeneratorTest.java
@@ -0,0 +1,26 @@
+package generator;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MyBatisGeneratorTest {
+
+    @Test
+    public void generatorTest() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(this.getClass().getClassLoader().getResourceAsStream("generator.xml"));
+
+        DefaultShellCallback shellCallback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+}
