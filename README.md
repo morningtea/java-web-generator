2163b93aebbb1f21feac10a42157079467453852
yepeng <yepeng@xsili.com>
2017-08-29 15:06:35 +0800

    新增自定义注释器XsiliCommentGenerator, 支持生成Model field,getter,setter的注释(读取数据库注释), 以及Mapper.xml"@mbg.generated"注释

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 本项目基于mybatis-generator-core进行构建, 主要更新如下:
 
 1. 添加了SpringMvcController, Service, MapperJunitTest三个代码生成插件
-2. 集成Mybatis分页插件 [xsili-mybatis-plugin-page](https://github.com/morningtea/xsili-mybatis-plugin-page "Mybatis分页插件")  
-3. 对原有功能进行了增强:  
-a) Model添加字段注释(对应数据库列的注释)  
-b) 为Mapper类添加@Repository注解(可配置)
+2. 集成Mybatis分页插件 [xsili-mybatis-plugin-page](https://github.com/morningtea/xsili-mybatis-plugin-page "Mybatis分页插件")
+4. 新增自定义注释器XsiliCommentGenerator, 支持生成Model field,getter,setter的注释(读取数据库注释), 以及Mapper.xml"@mbg.generated"注释
+3. 扩展MapperAnnotationPlugin, 添加对 annotationClass annotationName 配置支持. 可以配置为Mapper类添加@Repository注解
 
 #### Get started
 插件支持hsqldb数据库内存模式, 只需要执行 MyBatisGeneratorTest即可生成样例代码.  
@@ -17,9 +16,14 @@ b) 为Mapper类添加@Repository注解(可配置)
 2. 以junit的方式运行test/java/generator/MyBatisGeneratorTest.java
 3. 把生成的文件copy到自己的工程
 
-
 #### 其他说明
 1. src/site 是mybatis-generator-core-1.3.6的官方文档
 2. 支持MySQL, Oracle, PostgreSQL等数据库, 只要该数据库的JDBC驱动实现了DatabaseMetaData接口,
 特别是"getColumns"和"getPrimaryKeys"方法
+
+#### 兼容mybatis-generator-core:
+1. 扩展MapperAnnotationPlugin, 添加对 annotationClass annotationName 配置支持
+2. 扩展org.mybatis.generator.api.dom.java.Method, 添加方法removeBodyLines()
+3. 扩展SerializablePlugin, 为ExampleClass添加serialVersionUID (添加覆盖方法  @Override#modelExampleClassGenerated)
+以上修改, 均是功能扩展, 可以兼容MBG的原有功能和配置项.
 
---
+++ src/main/java/org/mybatis/generator/xsili/XsiliCommentGenerator.java
@@ -0,0 +1,252 @@
+package org.mybatis.generator.xsili;
+
+import static org.mybatis.generator.internal.util.StringUtility.isTrue;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.InnerClass;
+import org.mybatis.generator.api.dom.java.InnerEnum;
+import org.mybatis.generator.api.dom.java.JavaElement;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.MergeConstants;
+import org.mybatis.generator.config.PropertyRegistry;
+import org.mybatis.generator.internal.util.StringUtility;
+
+/**
+ * 
+ * @author 叶鹏
+ * @date 2017年8月29日
+ */
+public class XsiliCommentGenerator implements CommentGenerator {
+
+    private Properties properties;
+
+    private boolean suppressDate;
+
+    private boolean suppressAllComments;
+
+    /** If suppressAllComments is true, this option is ignored. */
+    private boolean addRemarkComments;
+
+    private SimpleDateFormat dateFormat;
+
+    public XsiliCommentGenerator() {
+        super();
+        properties = new Properties();
+        suppressDate = false;
+        suppressAllComments = false;
+        addRemarkComments = false;
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        // add no file level comments by default
+    }
+
+    /**
+     * Adds a suitable comment to warn users that the element was generated, and when it was generated.
+     *
+     * @param xmlElement the xml element
+     */
+    @Override
+    public void addComment(XmlElement xmlElement) {
+        if (suppressAllComments) {
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<!-- ");
+        sb.append(MergeConstants.NEW_ELEMENT_TAG);
+        sb.append(" -->");
+        xmlElement.addElement(new TextElement(sb.toString()));
+    }
+
+    @Override
+    public void addRootComment(XmlElement rootElement) {
+        // add no document level comments by default
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        this.properties.putAll(properties);
+
+        suppressDate = isTrue(properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_DATE));
+
+        suppressAllComments = isTrue(properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_ALL_COMMENTS));
+
+        addRemarkComments = isTrue(properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_ADD_REMARK_COMMENTS));
+
+        String dateFormatString = properties.getProperty(PropertyRegistry.COMMENT_GENERATOR_DATE_FORMAT);
+        if (StringUtility.stringHasValue(dateFormatString)) {
+            dateFormat = new SimpleDateFormat(dateFormatString);
+        }
+    }
+
+    /**
+     * This method adds the custom javadoc tag for. You may do nothing if you do not wish to include the Javadoc tag -
+     * however, if you do not include the Javadoc tag then the Java merge capability of the eclipse plugin will break.
+     *
+     * @param javaElement the java element
+     * @param markAsDoNotDelete the mark as do not delete
+     */
+    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
+        javaElement.addJavaDocLine(" * ");
+        StringBuilder sb = new StringBuilder();
+        String s = getDateString();
+        if (s != null) {
+            sb.append(" * @date ");
+            sb.append(s);
+        }
+        javaElement.addJavaDocLine(sb.toString());
+    }
+
+    /**
+     * Returns a formated date string to include in the Javadoc tag and XML comments. You may return null if you do not
+     * want the date in these documentation elements.
+     * 
+     * @return a string representing the current timestamp, or null
+     */
+    protected String getDateString() {
+        if (suppressDate) {
+            return null;
+        } else if (dateFormat != null) {
+            return dateFormat.format(new Date());
+        } else {
+            return new Date().toString();
+        }
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+        if (suppressAllComments) {
+            return;
+        }
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        if (suppressAllComments || !addRemarkComments) {
+            return;
+        }
+
+        topLevelClass.addJavaDocLine("/**");
+
+        String remarks = introspectedTable.getRemarks();
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                topLevelClass.addJavaDocLine(" * " + remarkLine);
+            }
+        }
+        addJavadocTag(topLevelClass, true);
+        topLevelClass.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+
+        innerEnum.addJavaDocLine("/**"); //$NON-NLS-1$
+        addJavadocTag(innerEnum, false);
+        innerEnum.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addFieldComment(Field field,
+                                IntrospectedTable introspectedTable,
+                                IntrospectedColumn introspectedColumn) {
+        if (suppressAllComments) {
+            return;
+        }
+
+        addCommentSingleLine(field, introspectedColumn);
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+        if (suppressAllComments) {
+            return;
+        }
+    }
+
+    @Override
+    public void addGetterComment(Method method,
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+        if (suppressAllComments) {
+            return;
+        }
+
+        addCommentSingleLine(method, introspectedColumn);
+    }
+
+    @Override
+    public void addSetterComment(Method method,
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+        if (suppressAllComments) {
+            return;
+        }
+
+        addCommentSingleLine(method, introspectedColumn);
+    }
+
+    @SuppressWarnings("unused")
+    private void addCommentMultiLine(JavaElement el, IntrospectedColumn introspectedColumn) {
+        el.addJavaDocLine("/**");
+
+        String remarks = introspectedColumn.getRemarks();
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                el.addJavaDocLine(" * " + remarkLine);
+            }
+        }
+
+        el.addJavaDocLine(" */");
+    }
+
+    private void addCommentSingleLine(JavaElement el, IntrospectedColumn introspectedColumn) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/** ");
+
+        String remarks = introspectedColumn.getRemarks();
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            String remarkJoinStr = "";
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+            for (String remarkLine : remarkLines) {
+                remarkJoinStr = remarkJoinStr + remarkLine + " ";
+            }
+            sb.append(remarkJoinStr);
+        }
+        sb.append("*/");
+        el.addJavaDocLine(sb.toString());
+    }
+
+}
