6dd68348e2ff428a95addc634f2d80358d977fd1
yepeng <yepeng@xsili.com>
2017-12-25 19:29:29 +0800

    repository逻辑删除方法添加更新日期参数

--- src/main/java/org/mybatis/generator/codegen/jpa2/repository/RepositoryGenerator.java
+++ src/main/java/org/mybatis/generator/codegen/jpa2/repository/RepositoryGenerator.java
@@ -35,6 +35,7 @@ import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMa
 import org.mybatis.generator.codegen.mybatis3.javamapper.elements.SelectByExampleWithoutBLOBsMethodGenerator;
 import org.mybatis.generator.config.PropertyRegistry;
 import org.mybatis.generator.xsili.Constants;
+import org.mybatis.generator.xsili.GenHelper;
 import org.mybatis.generator.xsili.plugins.util.PluginUtils;
 
 /**
@@ -88,37 +89,49 @@ public class RepositoryGenerator extends AbstractJavaClientGenerator {
 
         // 添加方法
         // 逻辑删除方法
-        String logicDeletedName = PluginUtils.getPropertyNotNull(getContext(), Constants.KEY_LOGIC_DELETED_NAME);
-        for(IntrospectedColumn column : introspectedTable.getAllColumns()) {
-            if(column.getJavaProperty().equals(logicDeletedName)) {
-                Method updateDeletedMethod = new Method();
-                updateDeletedMethod.setName("updateDeleted");
-                updateDeletedMethod.setReturnType(FullyQualifiedJavaType.getIntInstance());
-                
-                // 拼接hql
-                StringBuilder hql = new StringBuilder();
-                List<IntrospectedColumn> keyColumns = introspectedTable.getPrimaryKeyColumns();
-                hql.append("update ").append(modelType.getShortName());
-                hql.append(" set ").append(logicDeletedName + " = ?").append(keyColumns.size() + 1);
-                hql.append(" where ");
-                for (int i = 0; i < keyColumns.size(); i++) {
-                    IntrospectedColumn keyColumn = keyColumns.get(i);
-                    hql.append(keyColumn.getJavaProperty() + " = ?").append(i + 1);
-                    // 如果不是最后一个参数, 则拼接 and
-                    if (i + 1 < keyColumns.size()) {
-                        hql.append(" and ");
-                    }
-                    updateDeletedMethod.addParameter(new Parameter(keyColumn.getFullyQualifiedJavaType(), keyColumn.getJavaProperty()));
+        IntrospectedColumn logicDeletedColumn = GenHelper.getLogicDeletedField(introspectedTable);
+        if(logicDeletedColumn != null) {
+            Method updateDeletedMethod = new Method();
+            updateDeletedMethod.setName("updateDeleted");
+            updateDeletedMethod.setReturnType(FullyQualifiedJavaType.getIntInstance());
+            
+            // 拼接hql
+            StringBuilder hql = new StringBuilder();
+            List<IntrospectedColumn> keyColumns = introspectedTable.getPrimaryKeyColumns();
+            hql.append("update ").append(modelType.getShortName());
+            hql.append(" set ").append(logicDeletedColumn.getJavaProperty() + " = ?").append(keyColumns.size() + 1);
+            // 更新时间
+            Parameter updatedDateParameter = null;
+            String updatedDateName = PluginUtils.getPropertyNotNull(getContext(), Constants.KEY_UPDATED_DATE_NAME);
+            for (IntrospectedColumn updatedDateColumn : introspectedTable.getAllColumns()) {
+                if (updatedDateName.equals(updatedDateColumn.getJavaProperty())) {
+                    hql.append(", ").append(updatedDateColumn.getJavaProperty() + " = ?").append(keyColumns.size() + 2);
+                    
+                    interfaze.addImportedType(updatedDateColumn.getFullyQualifiedJavaType());
+                    updatedDateParameter = new Parameter(updatedDateColumn.getFullyQualifiedJavaType(), updatedDateColumn.getJavaProperty());
                 }
-                updateDeletedMethod.addParameter(new Parameter(FullyQualifiedJavaType.getBooleanPrimitiveInstance(), "isDeleted"));
-
-                updateDeletedMethod.addAnnotation("@Modifying");
-                updateDeletedMethod.addAnnotation("@Query(\"" + hql.toString() + "\")");
-                interfaze.addImportedType(annotationModifyingType);
-                interfaze.addImportedType(annotationQueryType);
-                
-                interfaze.addMethod(updateDeletedMethod);
             }
+            hql.append(" where ");
+            for (int i = 0; i < keyColumns.size(); i++) {
+                IntrospectedColumn keyColumn = keyColumns.get(i);
+                hql.append(keyColumn.getJavaProperty() + " = ?").append(i + 1);
+                // 如果不是最后一个参数, 则拼接 and
+                if (i + 1 < keyColumns.size()) {
+                    hql.append(" and ");
+                }
+                updateDeletedMethod.addParameter(new Parameter(keyColumn.getFullyQualifiedJavaType(), keyColumn.getJavaProperty()));
+            }
+            updateDeletedMethod.addParameter(new Parameter(FullyQualifiedJavaType.getBooleanPrimitiveInstance(), logicDeletedColumn.getJavaProperty()));
+            if(updatedDateParameter != null) {
+                updateDeletedMethod.addParameter(updatedDateParameter);
+            }
+            
+            updateDeletedMethod.addAnnotation("@Modifying");
+            updateDeletedMethod.addAnnotation("@Query(\"" + hql.toString() + "\")");
+            interfaze.addImportedType(annotationModifyingType);
+            interfaze.addImportedType(annotationQueryType);
+            
+            interfaze.addMethod(updateDeletedMethod);
         }
         
         // TODO 添加 listWithoutBLOBs
--- src/main/java/org/mybatis/generator/xsili/GenHelper.java
+++ src/main/java/org/mybatis/generator/xsili/GenHelper.java
@@ -5,14 +5,43 @@
 package org.mybatis.generator.xsili;
 
 import org.apache.commons.lang3.StringUtils;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.config.Context;
+import org.mybatis.generator.xsili.plugins.util.PluginUtils;
 
 /**
  * @author 叶鹏
  * @date 2017年12月15日
  */
 public class GenHelper {
+    
+    /**
+     * 是否包含逻辑删除列
+     * 
+     * @param introspectedTable
+     * @return
+     */
+    public static boolean hasLogicDeletedField(IntrospectedTable introspectedTable) {
+        return getLogicDeletedField(introspectedTable) != null;
+    }
+
+    /**
+     * 获取逻辑删除列
+     * 
+     * @param introspectedTable
+     * @return maybe null
+     */
+    public static IntrospectedColumn getLogicDeletedField(IntrospectedTable introspectedTable) {
+        String logicDeletedName = PluginUtils.getPropertyNotNull(introspectedTable.getContext(), Constants.KEY_LOGIC_DELETED_NAME);
+        for (IntrospectedColumn column : introspectedTable.getAllColumns()) {
+            if (column.getJavaProperty().equals(logicDeletedName)) {
+                return column;
+            }
+        }
+        return null;
+    }
 
     public static FullyQualifiedJavaType getBusinessExceptionType(Context context) {
         String businessExceptionName = context.getProperty(Constants.KEY_BUSINESS_EXCEPTION_QUALIFIED_NAME);
@@ -22,5 +51,7 @@ public class GenHelper {
             return new FullyQualifiedJavaType("java.lang.RuntimeException");
         }
     }
+    
+    
 
 }
